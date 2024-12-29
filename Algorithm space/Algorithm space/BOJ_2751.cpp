7a587fe399d0bc4ac86efb5a97a13f7082ec2060
Patrick-Gong <owlcountry01@gmail.com>
2024-12-29 18:24:03 +0900

    백준 2751 해결

--- Algorithm space/Algorithm space/BOJ_2751.cpp
+++ Algorithm space/Algorithm space/BOJ_2751.cpp
@@ -17,7 +17,7 @@ vector<int> vec;
     sort(vec.begin(), vec.end());
     
     for (int i = 0; i < N; i++) {
-        cout << vec[i] << endl;
+        cout << vec[i] << "\n";
     }
 
     return 0;
